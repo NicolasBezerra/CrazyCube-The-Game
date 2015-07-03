e4fe04b33448ec5bfb0a21fa02ca636b9cc797f2
nicolasbezerra14@gmail.com <Sabbath14>
2015-07-03 09:24:06 -0300

    Rotação do Cubo Grande
    - rotação feita com o pressionar do button direito e sua movimentação
    afim.

--- Assets/script/FaceScript.cs
+++ Assets/script/FaceScript.cs
@@ -7,6 +7,8 @@ public class FaceScript : MonoBehaviour
 	public catchBlock BlockS;
 	public Instanciador CatchColor;
 	private Vector3 scanPos;
+	private bool alreadyColored = false;
+
 	void Start () 
 	{
 		CatchColor = GameObject.Find("GameObject").GetComponent<Instanciador>();
@@ -14,28 +16,15 @@ public class FaceScript : MonoBehaviour
 	
 	void OnMouseUp() 
 	{
-		
+		if (CatchColor.Catching && !alreadyColored) {
+			gameObject.renderer.material.color = CatchColor.Mycolor;
+			CatchColor.Catching = false;
+			alreadyColored = true;
+		}
 	}
 	void Update () 
 	{
-		scanPos = transform.position;
-		screenPoint = Camera.main.WorldToScreenPoint(scanPos);
-		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		if(curScreenPoint == transform.position && Input.GetMouseButtonUp(1) && CatchColor.Catching)
-		{
-			gameObject.renderer.material.color = CatchColor.Mycolor;
-		}
-		/*try
-		{
-			if(GameObject.FindGameObjectWithTag("blocoUsing") != null)
-			{
-				BlockS = GameObject.FindGameObjectWithTag("blocoUsing").GetComponent<catchBlock>();
-			}
-		}
-		catch
-		{
-		
-		
-		}*/
+
+
 	}
 }
