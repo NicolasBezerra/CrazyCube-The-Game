fd0d6a9464673e9ced39df424cd8e13070bfe640
nicolasbezerra14@gmail.com <Sabbath14>
2015-07-03 09:13:38 -0300

    Nada de mais
    - Movimentando cubo com o mouse
    - Deletando o cubo em uma área
    - Largando cubo em um lugar

---
+++ Assets/script/MasterRotationFuck.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterRotationFuck : MonoBehaviour {
+
+	public float sensitivityX ;
+	public float sensitivityY ;
+	private Transform cameraTm;
+		
+	public bool down = false;
+	
+	// Use this for initializationz975
+	void Start ()
+	{
+		sensitivityX = 10.0f;
+		sensitivityY = 10.0f;
+		cameraTm = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetMouseButtonDown( 1 ) )
+			down = true;
+		else if( Input.GetMouseButtonUp( 1 ) )
+			down = false;
+		
+		if(down){
+			float rotationX = Input.GetAxis("Mouse X") * sensitivityX;
+			transform.RotateAround( cameraTm.up, -Mathf.Deg2Rad * rotationX );
+			float rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
+			transform.RotateAround( cameraTm.right, Mathf.Deg2Rad * rotationY );
+
+		}
+
+		/*if (down) {
+			float rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
+			transform.RotateAround( cameraTm.right, Mathf.Deg2Rad * rotationY );
+		}*/
+	}
+
+}
