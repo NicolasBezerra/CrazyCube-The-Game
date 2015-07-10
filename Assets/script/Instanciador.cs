6ad3be659b133289ede3ec4d17d7f3c60835fda6
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-07-10 18:52:20 -0300

    Revert "Quase Acabando para enviar // iremos continuar o projeto"
    This reverts commit 538510f3a776898687bbbd7a85a83ac029dff884.

---
+++ Assets/script/Instanciador.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instanciador : MonoBehaviour {
+	private Vector3 screenPoint;
+	private GameObject bloco_a;
+	private GameObject bloco_am;
+	private GameObject bloco_v;
+	private GameObject bloco_ve;
+	private GameObject bloco_b;
+	private GameObject bloco_l;
+	private Vector3 lugar;
+	private float tempo = 1;
+	private float segundos;
+	public Material Mycolor;
+	public bool Catching = false;	
+	private int myInt;
+	public float CountTo;
+	public bool coloca;
+	
+	// Use this for initialization
+	void Start () {
+		screenPoint = Camera.main.WorldToScreenPoint(new Vector3(0,0,0));
+
+		lugar = Camera.main.ScreenToWorldPoint (new Vector3 (Camera.main.pixelWidth, Camera.main.pixelHeight/10f, screenPoint.z));
+		bloco_a = Resources.Load ("CubeAzul") as GameObject;
+		bloco_am = Resources.Load ("CubeAmarelo") as GameObject;
+		bloco_v = Resources.Load ("CubeVermelho") as GameObject;
+		bloco_ve = Resources.Load ("CubeVerde") as GameObject;
+		bloco_b = Resources.Load ("CubeBranco") as GameObject;
+		bloco_l = Resources.Load ("CubeLaranja") as GameObject;
+		//Instantiate (bloco_pf , lugar , Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		try
+		{
+			if(GameObject.FindGameObjectWithTag("blocoExit")!= null)
+			{
+				CountTo += Time.deltaTime;
+				if(CountTo > 0.002f)
+				{
+					CountTo = 0;
+					Destroy(GameObject.FindGameObjectWithTag("blocoExit") );
+				}
+			}
+			else if(coloca && Catching)
+			{
+				CountTo += Time.deltaTime;
+				if(CountTo > 0.004f)
+				{
+					coloca = false;
+					Catching = false;
+				}
+			}
+		}
+		catch
+		{
+			
+		}
+		segundos = Time.realtimeSinceStartup;
+		//Debug.Log (Time.realtimeSinceStartup);
+		if(segundos > tempo)
+		{
+			myInt = Mathf.FloorToInt(Random.Range(0,6));
+			switch(myInt)
+			{
+				case 0:
+					Instantiate (bloco_a , lugar , Quaternion.identity);
+				break;
+				case 1:
+					Instantiate (bloco_am , lugar , Quaternion.identity);
+				break;
+				case 2:
+					Instantiate (bloco_v , lugar , Quaternion.identity);
+				break;
+				case 3:
+					Instantiate (bloco_ve , lugar , Quaternion.identity);
+				break;
+				case 4:
+					Instantiate (bloco_l , lugar , Quaternion.identity);
+				break;
+				case 5:
+					Instantiate (bloco_b , lugar , Quaternion.identity);
+				break;
+			}
+			tempo += 1f;
+		}
+		
+	}
+}
---
+++ Assets/script/MasterRotationFuck.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterRotationFuck : MonoBehaviour {
+
+	public float sensitivityX ;
+	public float sensitivityY ;
+//	private Transform cameraTm;
+	private float rotationVeriX;
+	private float rotationX;
+	private float rotationVeriY;
+	private float rotationY;
+	public bool down = false;
+	
+	// Use this for initializationz975
+	void Start ()
+	{
+		sensitivityX = 300.0f;
+		sensitivityY = 300.0f;
+		//cameraTm = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetMouseButtonDown( 1 ) )
+			down = true;
+		else if( Input.GetMouseButtonUp( 1 ) )
+			down = false;
+		
+		if(down){
+			transform.Rotate((Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime), (Input.GetAxis("Mouse X") * -sensitivityX * Time.deltaTime), 0, Space.World);/*
+			rotationVeriX = rotationX;
+			rotationX = Input.GetAxis("Mouse X") * sensitivityX;
+			rotationVeriY = rotationY;
+			rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
+			if(rotationVeriX != rotationX)
+			{
+				//if(transform.localEulerAngles.y != 0)
+				//{
+					transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,transform.localEulerAngles.y,0);
+				//}
+				transform.RotateAround(cameraTm.up, -Mathf.Deg2Rad * rotationX);
+			}
+			if(rotationVeriY != rotationY)
+			{
+				//if(transform.localEulerAngles.x != 0)
+				//{
+				transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,transform.localEulerAngles.y,0f);
+				//}
+				transform.RotateAround(cameraTm.right, Mathf.Deg2Rad * rotationY/2);
+			}*/
+		}
+		/*if (down) {
+			float rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
+			transform.RotateAround( cameraTm.right, Mathf.Deg2Rad * rotationY );
+		}*/
+	}
+
+}
