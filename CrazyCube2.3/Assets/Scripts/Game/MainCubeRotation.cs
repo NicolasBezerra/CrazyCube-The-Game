c5fa11053ba09ffc51f50e95facfb18b675dd6d1
nicolasbezerra14@gmail.com <Sabbath14>
2015-07-13 11:15:34 -0300

    Finalizando a demo.
    - concertos dos bugs do menu
    - deixando a esteira mais rapida quando vc aperar no buton
    - rotação dos cubos iguais agora

---
+++ CrazyCube2.3/Assets/Scripts/Game/MainCubeRotation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCubeRotation : MonoBehaviour 
+{
+	public float sensitivityX ;
+	public float sensitivityY ;
+    private bool Down;
+	void Start ()
+	{
+        Down = false;
+		sensitivityX = 300.0f;
+		sensitivityY = 300.0f;
+	}
+	void Update () 
+    {
+		if(Input.GetMouseButtonDown(1))
+        {
+            Down = true;
+        }	
+		else if(Input.GetMouseButtonUp(1))
+        {
+            Down = false;
+        }
+        if (Down && Application.platform == RuntimePlatform.WindowsPlayer || Down && Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if(GameObject.FindGameObjectWithTag("blocoUsing") == null)
+            {
+                transform.Rotate((Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime), (Input.GetAxis("Mouse X") * -sensitivityX * Time.deltaTime), 0, Space.World);
+            }
+		}
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            if (GameObject.FindGameObjectWithTag("blocoUsing") == null)
+            {
+                transform.Rotate((Input.GetTouch(0).deltaPosition.y * sensitivityY / 10 * Time.deltaTime), (Input.GetTouch(0).deltaPosition.x * -sensitivityX / 10 * Time.deltaTime), 0, Space.World);
+                transform.Rotate(0, 0, ((Input.GetTouch(1).deltaPosition.x * -sensitivityX / 10 * Time.deltaTime) + (Input.GetTouch(1).deltaPosition.y * -sensitivityY / 10 * Time.deltaTime)) / 2, Space.World);
+            }
+        }
+        if(Down == false || Input.GetMouseButton(0) == false || Input.GetMouseButtonDown(0) == false)
+        {
+            transform.Rotate(0.01f, 0.01f, 0.01f);
+        }
+	}
+}
